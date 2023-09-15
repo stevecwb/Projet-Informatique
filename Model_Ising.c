6bb6e74caa20a13e0c1f3598c86b6749358361d0
Estevito <e168030@dac.unicamp.br>
2023-09-15 17:07:49 +0200

    end of the week

--- Model_Ising.c
+++ Model_Ising.c
@@ -5,6 +5,31 @@
 #include <stdbool.h>
 #include <string.h>
 
+double analytical_energy_1d(double T){
+    double J = 1;
+    double kb = 1;
+    double E = -J*tanh(J / (kb*T));
+    return E;
+}
+double analytical_heat_capacity_1d(double T){
+    double J = 1;
+    double kb = 1;
+    double Cv = ((pow(J, 2))/(kb*pow(T, 2)))*pow(1.0/cosh((J)/(kb*T)), 2);
+    return Cv;
+}
+
+int solution_exacte_1d(double table[3][100]){
+    for(int i=0; i <=100; i++){
+        double T = i/100;
+        double E = analytical_energy_1d(T);
+        double Cv = analytical_heat_capacity_1d(T);
+        table[0][i] = T;
+        table[1][i] = E;
+        table[2][i] = Cv;
+        printf("%lf\t%lf\t%lf\n", table[0][i],table[1][i], table[2][i]);
+    }
+    return 0;
+}
 double energie_du_systeme_1d(double J, int N, double matrix[1][N])
 {
     double E = 0;
@@ -15,13 +40,10 @@ double energie_du_systeme_1d(double J, int N, double matrix[1][N])
     E = E + matrix[0][0] * matrix[0][N - 1]; // boundary conditions
     return -J * E;
 }
-int methode_ising_1d(int N, int n_iter, double J, double kb)
+int methode_ising_1d(int N, int n_iter, double J, double kb, double results[3][100])
 {
     // nombre de spins
-    // nombre d'iterations
-    // constant d'Energie generique
-    // constant de Boltzman
-    double results[3][20];
+    //double results[3][20];
 
     // Open a file for writing (you can change "output.txt" to your desired file name)
     FILE *file = fopen("Resultats por 1 dimension.csv", "w");
@@ -59,7 +81,7 @@ int methode_ising_1d(int N, int n_iter, double J, double kb)
         }
         printf("\n");
     }*/
-    for (int T = 0; T <= 20; T++)
+    for (int T = 0; T < 100; T++)
     {
         double matrix[1][N];
         for (int i = 0; i < N; i++)
@@ -108,9 +130,36 @@ int methode_ising_1d(int N, int n_iter, double J, double kb)
 }
 
 
+
+void writeCSV(const char *filename, int rows, int cols, double table[7][100]) {
+    FILE *file = fopen(filename, "w");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    // Write column headers to the CSV file (if needed)
+    fprintf(file, "T (K), Energie Exacte(J), Capacite Calorifique Exacte (J/K), Magnetisation Exacte (A/m), Energie Numerique (J), Capacite Calorifique Numerique (J/K), Magnetisation Numerique (A/m)\n"); // Modify column headers as needed
+
+    // Write table values to the CSV file
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            fprintf(file, "%d", table[i][j]);
+
+            // Add a comma if it's not the last column
+            if (j < cols - 1) {
+                fprintf(file, ", ");
+            }
+        }
+        fprintf(file, "\n"); // Move to the next line
+    }
+
+    fclose(file);
+}
 int main(){
-    double J = 1;
-    double kb = 1;
+    double J = 1; // constant d'Energie generique
+    double kb = 1; // constant de Boltzman
     char dimension[2];
     printf("Dans quelle dimension souhaitez-vous simuler (1D, 2D ou 3D) ?");
     scanf("%s", &dimension);
@@ -119,7 +168,22 @@ int main(){
         printf("En 1 dimension, vous devez simplement saisir le nombre de spins : ");
         scanf("%d", &N);
         int n_iter = 500*N;
-        methode_ising_1d(N, n_iter, J, kb);
+        double matrice_analytique[3][100];
+        double matrice_numerique[3][100];
+        double final_matrice[7][100];
+        methode_ising_1d(N, n_iter, J, kb, matrice_numerique);
+        solution_exacte_1d(matrice_analytique);
+        for(int i = 0; i < 100; i++){
+            final_matrice[0][i] = matrice_analytique[0][i];
+            final_matrice[1][i] = matrice_analytique[1][i];
+            final_matrice[2][i] = matrice_analytique[2][i];
+            final_matrice[3][i] = 0;
+            final_matrice[4][i] = matrice_numerique[1][i];
+            final_matrice[5][i] = matrice_numerique[2][i];
+            final_matrice[6][i] = 0;
+        }
+        writeCSV("Teste de Arquivo.csv", 100, 7, final_matrice);
+
     }
     else if(strcmp(dimension, "2D") == 0){
         printf("2D");
@@ -131,7 +195,5 @@ int main(){
         printf("Vous devez avoir mal tape. Reessayez avec les options suivantes : 1D, 2D et 3D.");
         return 0;
     }
-    //printf("cets %s", dimension);
-    //methode_ising_1d();
     return 0;
 }
